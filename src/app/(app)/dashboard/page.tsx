125579c9915721b4f6fa58559cffcf56379c69f3
m0nster-mash <jacejpalmer@gmail.com>
2026-08-26 10:28:32 -0700

    Reverted broken file

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,15 +1,8 @@
 export default function DashboardPage() {
     return (
         <div>
-			<header>
-				<h1>This is the logged in screen that should appear on every logged in page.</h1>
-				<p>Signed in as {session.user.email}</p>
-				 <div>
-                	<Link href="/styletest">Click here to see the style test page.</Link>
-            	</div>
-				<SignOutButton />
-			</header>
-			<main>{children}</main>
+        	<h1>This is the dashboard that should appear on every logged in page.</h1>
+			<p>Dashboard</p>
 		</div>
     );
 }
