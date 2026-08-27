ddb6bea8dd3543192000b773a177cdd36236f426
m0nster-mash <jacejpalmer@gmail.com>
2026-08-27 09:10:33 -0700

    PROJ-010: reverting layout changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import {headers} from "next/headers";
 import {auth} from "@/core/auth";
 import {AuthCard} from "@/core/auth";
 import DashboardPage from "./dashboard/page";
-import SessionLayout from "@/app/components/session-layout";
 
 export default async function Home() {
     const session = await auth.api.getSession({
@@ -12,7 +11,6 @@ export default async function Home() {
 
     return (
         <main>
-            <SessionLayout>
                 <h1>go away</h1>
                 <div>
                     {session?.user ? (
@@ -24,7 +22,6 @@ export default async function Home() {
                 <div>
                     <Link href="/styletest">Click here to see the style test page.</Link>
                 </div>
-            </SessionLayout>
         </main>
     );
 }
