301fbbbdea72dba08d558c4a2661d0e5e9402c69
m0nster-mash <jacejpalmer@gmail.com>
2026-09-01 16:15:13 -0700

    Updated schema

---
+++ src/feature/avatar/schema/avatar.schema.ts
@@ -0,0 +1,27 @@
+import {relations} from "drizzle-orm";
+import {jsonb, pgTable, text, timestamp} from "drizzle-orm/pg-core";
+import {user} from "@/core/auth/schema/auth.schema";
+import {AvatarConfig} from "@/core/avatar/lib/types";
+
+export const avatarConfig =
+    pgTable("avatar_config", {
+        userId: text("user_id")
+            .primaryKey()
+            .references(() => user.id, {onDelete: "cascade"}),
+        config: jsonb("config")
+            .$type<AvatarConfig>(),
+        createdAt: timestamp("created_at")
+            .defaultNow()
+            .notNull(),
+        updatedAt: timestamp("updated_at")
+            .defaultNow()
+            .$onUpdate(() => /* @__PURE__ */ new Date())
+            .notNull(),
+    });
+
+export const avatarConfigRelations = relations(avatarConfig, ({one}) => ({
+    user: one(user, {
+        fields: [avatarConfig.userId],
+        references: [user.id],
+    }),
+}));
